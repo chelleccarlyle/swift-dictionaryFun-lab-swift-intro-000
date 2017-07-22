302c5acab5b39be53f443064431d028dbf7c8e71
chelleccarlyle <chelle_carlyle@yahoo.com>
2017-07-21 22:13:51 -0700

    Finish ice cream lab which utilizes dictionary methods

--- Icecream/Icecream.swift
+++ Icecream/Icecream.swift
@@ -9,28 +9,72 @@
 class Icecream {
     
     // 1. Create the Dictionary here. The name of the variable should be favoriteFlavorsOfIceCream
-    
+    var favoriteFlavorsOfIceCream: [String: String] = ["Joe": "Peanut Butter and Chocolate",
+                                                       "Tim": "Natural Vanilla",
+                                                       "Sophie": "Mexican Chocolate",
+                                                       "Deniz": "Natural Vanilla",
+                                                       "Tom": "Mexican Chocolate",
+                                                       "Jim": "Natural Vanilla",
+                                                       "Susan": "Cookies 'N' Cream"]
     
     
     
     
     // 2.
-    
+    func names(forFlavor flavor: String) -> [String] {
+        
+        var names: [String] = []
+        
+        for (name, favFlavor) in favoriteFlavorsOfIceCream {
+            
+            if favFlavor == flavor {
+                names.append(name)
+            }
+            
+        }
+        
+        return names
+        
+    }
     
     
     
     
     
     // 3.
-    
-    
+    func count(forFlavor flavor: String) -> Int {
+        
+        var count = 0
+        
+        for (_, favFlavor) in favoriteFlavorsOfIceCream {
+            
+            if favFlavor == flavor {
+                count += 1
+            }
+            
+        }
+        
+        return count
+        
+    }
     
     
     
     
     
     // 4.
-   
+    func flavor(forPerson person: String) -> String? {
+        
+        let flavor = favoriteFlavorsOfIceCream[person]
+        
+        if let flavor = flavor {
+            return flavor
+        }
+        else {
+            return nil
+        }
+        
+    }
     
     
     
@@ -38,7 +82,25 @@ class Icecream {
     
     
     // 5.
-   
+    func replace(flavor: String, forPerson person: String) -> Bool {
+        
+        for human in favoriteFlavorsOfIceCream.keys {
+            
+            //Person exists
+            if person == human {
+                
+                //Replace value
+                favoriteFlavorsOfIceCream[person] = flavor
+                
+                return true
+                
+            }
+            
+        }
+        
+        return false
+        
+    }
     
     
     
@@ -46,7 +108,18 @@ class Icecream {
     
     
     // 6.
-    
+    func remove(person: String) -> Bool {
+        
+        let old = favoriteFlavorsOfIceCream.removeValue(forKey: person)
+        
+        if old != nil {
+            return true
+        }
+        else {
+            return false
+        }
+        
+    }
     
     
     
@@ -55,7 +128,11 @@ class Icecream {
     
     
     // 7.
-    
+    func numberOfAttendees() -> Int {
+        
+        return favoriteFlavorsOfIceCream.count
+        
+    }
     
     
     
@@ -64,7 +141,22 @@ class Icecream {
     
     
     // 8.
-    
+    func add(person: String, withFlavor flavor: String) -> Bool {
+        
+        for (human, _) in favoriteFlavorsOfIceCream {
+            
+            //Person exists, do not update
+            if human == person {
+                return false
+            }
+            
+        }
+        
+        //Safe to add
+        favoriteFlavorsOfIceCream[person] = flavor
+        return true
+        
+    }
     
     
     
@@ -74,7 +166,32 @@ class Icecream {
     
     
     // 9.
-    
+    func attendeeList() -> String {
+        
+        var list = ""
+       
+        let allNames = Array(favoriteFlavorsOfIceCream.keys).sorted()
+        
+        for name in allNames {
+            
+            let flavor = favoriteFlavorsOfIceCream[name]
+            
+            if let flavor = flavor {
+                
+                if allNames.last == name {
+                    list += "\(name) likes \(flavor)"
+                }
+                else {
+                    list += "\(name) likes \(flavor)\n"
+                }
+                
+            }
+            
+        }
+        
+        return list
+        
+    }
     
     
     
